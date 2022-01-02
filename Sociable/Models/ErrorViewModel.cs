c7d40543d21c7d40c2f58f1c17fe777a87e45d02
Iliyan <ili_kop3@abv.bg>
2022-01-02 15:01:35 +0200

    Adding project into git repo

---
+++ Sociable/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Sociable.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
